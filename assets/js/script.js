83610caad3b76cadaf01e94b8ee7ac8696c615c4
imjustSahen <114109203+imjustSahen@users.noreply.github.com>
2022-11-13 22:45:54 -0800

    adding files to GitHub

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Create Variables
+
+// Create Start Quiz Function
+
+// Get Questions Function
+
+// Check if Answer is right or wrong Function
+
+// End Quiz Function
+
+// Timer Function
+
+// Save highscore Function
